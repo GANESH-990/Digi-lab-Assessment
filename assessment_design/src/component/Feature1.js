b72bea7e01752bdda99df3d0fd9886ba2491856c
GANESH-990 <gdhage86@gmail.com>
2024-01-11 13:31:28 +0530

    Added Feature1 UI

--- assessment_design/src/component/Feature1.js
+++ assessment_design/src/component/Feature1.js
@@ -1,7 +1,162 @@
-export default function Feature1(){
-    return(
-        <>
-        <p>feature 1 component..</p>
-        </>
-    )
+export default function Feature1() {
+  return (
+    <>
+      <div className="px-lg-c5 py-lg-c6 px-sm-c2 py-sm-c5 px-c1 py-c4">
+        <div className=" px-sm-c2 ">
+
+
+          <div className="d-flex flex-column gap-lg-c5 gap-c3">
+
+
+
+            <div className="col-12">
+              <div className="col-lg-8 mx-auto d-flex flex-column gap-c2">
+
+
+                <div className=" d-flex flex-column gap-c1">
+                  <div
+                    style={styles._title}
+                    className="text-center px-lg-12d5pt text-Neutral50"
+                  >
+                    Elevating Card Programs with Cutting-Edge Technology
+                  </div>
+                  <div
+                    style={styles._subtitle}
+                    className="text-center text-Neutral400 px-lg-12d5pt col-11 mx-auto"
+                  >
+                    consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua.
+                  </div>
+                </div>
+
+                <div>
+                  <div className="d-flex gap-f2 justify-content-center align-items-center">
+                    <span className="d-flex align-items-center text-PriPurple400">
+                      Compare all Pro features
+                    </span>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="21"
+                      height="20"
+                      viewBox="0 0 21 20"
+                      fill="none"
+                    >
+                      <path
+                        d="M4.66666 9.99996H16.3333M16.3333 9.99996L10.5 4.16663M16.3333 9.99996L10.5 15.8333"
+                        stroke="#582066"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+            <div className="d-flex flex-lg-row flex-column align-items-center gap-c2 ">
+
+
+
+                <div className=" w-100  p-c1">
+
+
+                    <div className="d-flex flex-column gap-c1 align-items-center col-lg-12 col-sm-8 col-12 mx-auto">
+                        <div className=" d-flex align-items-center p-c1 border rounded-circle bg-PriPurple900">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<path d="M22 12C22 17.5228 17.5228 22 12 22M22 12C22 6.47715 17.5228 2 12 2M22 12H2M12 22C6.47715 22 2 17.5228 2 12M12 22C14.5013 19.2616 15.9228 15.708 16 12C15.9228 8.29203 14.5013 4.73835 12 2M12 22C9.49872 19.2616 8.07725 15.708 8 12C8.07725 8.29203 9.49872 4.73835 12 2M2 12C2 6.47715 6.47715 2 12 2" stroke="#582066" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                        </div>
+                        <div className=" d-flex flex-column align-items-center gap-c1 ">
+                            <span className="text-center text-Neutral50" style={styles._heading3}>Globally Accepted</span>
+                            <span className="text-center text-Neutral400" style={styles._heading4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="w-100  p-c1">
+                    <div className="d-flex flex-column gap-c1 align-items-center   mx-auto col-lg-12 col-sm-8 col-12">
+                        <div className=" d-flex align-items-center p-c1 border rounded-circle bg-PriPurple900">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M21 2L19 4M19 4L22 7L18.5 10.5L15.5 7.5M19 4L15.5 7.5M11.39 11.61C11.9063 12.1195 12.3168 12.726 12.5978 13.3948C12.8787 14.0635 13.0246 14.7813 13.027 15.5066C13.0295 16.232 12.8884 16.9507 12.6119 17.6213C12.3354 18.2919 11.9291 18.9012 11.4161 19.4141C10.9032 19.9271 10.2939 20.3334 9.6233 20.6099C8.95268 20.8864 8.234 21.0275 7.50863 21.025C6.78327 21.0226 6.06554 20.8767 5.39679 20.5958C4.72804 20.3148 4.12147 19.9043 3.612 19.388C2.61013 18.3507 2.05576 16.9614 2.06829 15.5193C2.08082 14.0772 2.65925 12.6977 3.679 11.678C4.69874 10.6583 6.07821 10.0798 7.52029 10.0673C8.96238 10.0548 10.3517 10.6091 11.389 11.611L11.39 11.61ZM11.39 11.61L15.5 7.5" stroke="#582066" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                        </div>
+                        <div className=" d-flex flex-column align-items-center gap-c1 ">
+                            <span className="text-center text-Neutral50" style={styles._heading3}>Biometric Integrated</span>
+                            <span className="text-center text-Neutral400" style={styles._heading4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className=" w-100  p-c1">
+                    <div className="d-flex flex-column gap-c1 align-items-center  mx-auto col-lg-12 col-sm-8 col-12">
+                        <div className=" d-flex align-items-center p-c1 border rounded-circle bg-PriPurple900">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<path d="M7 11V7C7 5.67392 7.52678 4.40215 8.46447 3.46447C9.40215 2.52678 10.6739 2 12 2C13.3261 2 14.5979 2.52678 15.5355 3.46447C16.4732 4.40215 17 5.67392 17 7V11M5 11H19C20.1046 11 21 11.8954 21 13V20C21 21.1046 20.1046 22 19 22H5C3.89543 22 3 21.1046 3 20V13C3 11.8954 3.89543 11 5 11Z" stroke="#582066" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                        </div>
+                        <div className=" d-flex flex-column align-items-center gap-c1">
+                            <span className="text-center text-Neutral50" style={styles._heading3}>Fully Secured</span>
+                            <span className="text-center text-Neutral400" style={styles._heading4}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod </span>
+                        </div>
+                    </div>
+                </div>
+
+
+
+       
+
+            </div>
+
+
+
+
+
+
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+const styles = {
+  _title: {
+    fontFamily: "Manrope",
+    fontSize: "40px",
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: "48px",
+  },
+  _subtitle: {
+    fontFamily: "Inter",
+    fontSize: "18px",
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "28px",
+  },
+
+  _heading3:{
+    fontFamily: "Inter",
+    fontSize: "16px",
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: "20px",
+
+  },
+
+  _heading4:{
+    fontFamily: "Inter",
+    fontSize: "14px",
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: "24px",
+
+  }
+};
